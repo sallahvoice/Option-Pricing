b22c95458d67003bf247d7a0df2a3135af52be5b
sallahvoice <sallahvoice@gmail.com>
2026-01-04 09:55:14 +0100

    basic logger & exception classes

--- exceptions.py
+++ exceptions.py
@@ -1,1 +1,28 @@
-#3-4 exceptions max
+class FailedToFetchData(Exception):
+    def __init__(self, message: str, value: str) -> None:
+        super().__init__(message)
+        self.message = message
+        self.value = value
+    
+    def __str__(self) -> str:
+        return f"{self.message}, (value : {self.value})"
+
+
+class TickerNotFound(Exception):
+    def __init__(self, message: str, value: str) -> None:
+        super().__init__(message)
+        self.message = message
+        self.value = value
+    
+    def __str__(self) -> str:
+        return f"{self.message}, (value : {self.value})"
+
+
+class QueryError(Exception):
+    def __init__(self, message: str, value: str) -> None:
+        super().__init__(message)
+        self.message = message
+        self.value = value
+    
+    def __str__(self) -> str:
+        return f"{self.message}, (value : {self.value})"
---
+++ logger.py
@@ -0,0 +1,37 @@
+import logging
+from logging.handlers import RotatingFileHandler
+from pathlib import Path
+
+log_path = Path(__file__).parent / "logs.log"
+log_path.parent.mkdir(parents=True, exist_ok=True) #not needed in this simple project btw
+
+
+def setup_logger(name: str = __name__):
+    logger = logging.getLogger(name)
+    logger.propagate = False #stops log records from propagating to parent/root loggers
+    logger.setLevel(logging.INFO)
+
+    if not logger.handlers:
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+
+        file_handler = RotatingFileHandler(
+            log_path,
+            maxBytes=2 * 1024 * 1024, 
+            backupCount=3,
+            encoding="utf-8",
+        )
+        file_handler.setFormatter(formatter)
+
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(formatter)
+
+        logger.addHandler(file_handler)
+        logger.addHandler(stream_handler)
+
+    return logger
+
+
+def get_logger(name: str):
+    return setup_logger(name)
