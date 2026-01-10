54313b48cfa8939538c1d1138449b9660d3d8d98
sallahvoice <sallahvoice@gmail.com>
2026-01-10 01:13:33 +0100

    d1 & d2 & there norm cdfs, call & put deltas & gammas

--- main/logic.py
+++ main/logic.py
@@ -4,6 +4,8 @@ from repositories.input_repo import InputRepository
 from repositories.output_repo import OutputRepository
 from logger import get_logger
 from typing import Dict
+from numpy import exp, sqrt, log
+from scipy.stats import norm
 
 logger = get_logger(__file__)
 run_migration()
@@ -22,16 +24,28 @@ def price_option(inputs: Dict[str, float]):
     strike_price = inputs["StrikePrice"]
     time = inputs["TimeToExpiry"]
     rf = inputs["RiskFreeRate"]
-    vol = inputs["Volatility"]
+    vol = inputs["Volatility"] #std
 
-    d1 = 
-    d2 =
-    nd1 = 
-    nd2 = 
-    call_price = 
-    put_price =  
+    d1 = (log(stock_price / strike_price) 
+        + (rf + 0.5 * vol**2) * time) / (vol * sqrt(time))
 
+    d2 = d1 - vol * sqrt(time)
 
+    call_price = (
+        stock_price * norm.cdf(d1)
+        - strike_price * exp(-rf * time) * norm.cdf(d2)
+    )
+
+    put_price = (
+        strike_price * exp(-rf * time) * norm.cdf(-d2)
+        - stock_price * norm.cdf(-d1)
+    )
+
+    call_delta = norm.cdf(d1)
+    put_delta = norm.cdf(d1) - 1
+
+    call_gamma = norm.pdf(d1)/(stock_price*vol*sqrt(time_to_expiry))
+    put_gamma = call_gamma
 
 if __name__ == "__main__":
     inputs = {"StockPrice": 50,
