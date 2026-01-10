28268609f15ae139dc0f5e72ba5e5dd4ed87afd4
sallahvoice <sallahvoice@gmail.com>
2026-01-10 12:32:32 +0100

    Fix db/config.py

--- db/config.py
+++ db/config.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 #default values
 host = os.getenv("DB_HOST")
-port = int(os.getenv("DB_PORT"))
+port = int(os.getenv("DB_PORT", "3306"))
 user = os.getenv("DB_USER")
 password = os.getenv("DB_PASSWORD")
 database = os.getenv("DB_NAME")
-database_pool_size = int(os.getenv("DB_POOL_SIZE"))
+database_pool_size = int(os.getenv("DB_POOL_SIZE", "5"))
 database_pool_name = os.getenv("DB_POOL_NAME")
